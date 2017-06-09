5f6ddd1148c028ca307ac68263f10e5ca0f417fc
Melobeat <melobeat@users.noreply.github.com>
2017-06-09 23:24:35 +0200

    Added URL Base64 Decoding

--- src/Exercise2/PaddingOracle.java
+++ src/Exercise2/PaddingOracle.java
@@ -1,8 +1,14 @@
 package Exercise2;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.math.BigInteger;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.URLDecoder;
+import java.util.ArrayList;
+import java.util.Base64;
+import java.util.List;
 
 /**
  * Project: IT-Security
@@ -10,12 +16,54 @@ import java.net.URL;
  */
 public class PaddingOracle {
 
+    private static final char[] HEX_ARRAY = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
+
     public static void main(String[] args) {
-        System.out.println(sendRequest("Test"));
-        System.out.println(sendRequest("NkivYeRHPWDYVIG%2FgiptBChbA8%2BZtjWslmOGB58oathyv1U13KIYy4kVC3Wuq4LcahaFL8lrxTl76VU921AVJw%3D%3D"));
+
+        String input = "NkivYeRHPWegVIG%2FgiptBChbA8%2BZtjWslmOGB58oathyv1U13KIYy4kVC3Wuq4LcahaFL8lrxTl76VU921AVJw%3D%3D";
+        List<String> cipherBlocks = getCiphertext(input);
+        System.out.println(cipherBlocks);
+
+        switch (sendRequest(input)) {
+            case 200:
+                System.out.println("Padding valid");
+                break;
+            case 500:
+                System.out.println("Padding invalid");
+                break;
+            case 400:
+                System.out.println("Bad request");
+                break;
+            default:
+                System.out.println("Something went wrong");
+        }
+
     }
 
-    private static int sendRequest(String query){
+    private static List<String> getCiphertext(String query) {
+
+        try {
+            String decodedURL = URLDecoder.decode(query, "UTF-8");
+            byte[] decodedBase64 = Base64.getDecoder().decode(decodedURL);
+
+            String text = String.format("%040x", new BigInteger(1, decodedBase64));
+            final int SIZE = 32;
+
+            List<String> ret = new ArrayList<>((text.length() + SIZE - 1) / SIZE);
+
+            for (int start = 0; start < text.length(); start += SIZE) {
+                ret.add(text.substring(start, Math.min(text.length(), start + SIZE)));
+            }
+            return ret;
+
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+    }
+
+    private static int sendRequest(String query) {
         final String SERVER = "gruenau5.informatik.hu-berlin.de";
         final String PROTOCOL = "http";
         final int PORT = 8888;
