ef9faf6bf417ea3214a1b49560c909a539d0f3e5
Melobeat <melobeat@users.noreply.github.com>
2017-06-10 17:37:32 +0200

    Some cleaning

--- src/Exercise2/PaddingOracle.java
+++ src/Exercise2/PaddingOracle.java
@@ -15,26 +15,31 @@ import java.util.Base64;
  */
 public class PaddingOracle {
 
+    private static final int BLOCKSIZE = 16;
+
     public static void main(String[] args) {
         String urlQuery = "NkivYeRHPWegVIG%2FgiptBChbA8%2BZtjWslmOGB58oathyv1U13KIYy4kVC3Wuq4LcahaFL8lrxTl76VU921AVJw%3D%3D";
 
+        byte[] randomR = new byte[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
         byte[][] cipherBlocks = splitArray(decode(urlQuery));
 
-        System.out.println(encode(concatArrays(cipherBlocks)));
+        byte[] r = searchR(randomR, cipherBlocks[cipherBlocks.length-1], BLOCKSIZE - 1);
 
-//        switch (sendRequest(input)) {
-//            case 200:
-//                System.out.println("Padding valid");
-//                break;
-//            case 500:
-//                System.out.println("Padding invalid");
-//                break;
-//            case 400:
-//                System.out.println("Bad request");
-//                break;
-//            default:
-//                System.out.println("Something went wrong");
-//        }
+        int paddingLength = findPaddingLength(r, cipherBlocks[cipherBlocks.length-1]);
+
+        byte[] a = new byte[BLOCKSIZE];
+
+        for (int i = a.length - 1; i > (a.length - 1) - paddingLength; i--) {
+            a[i] = (byte) (0xff & ((int) r[i]) ^ paddingLength);
+        }
+        System.out.println(Arrays.toString(a));
+
+        for (int i = r.length - 1; i > (r.length - 1) - paddingLength; i--) {
+            r[i] = (byte) (0xff & ((int) a[i]) ^ (paddingLength + 1));
+        }
+        System.out.println(Arrays.toString(r));
+
+        r = searchR(r, cipherBlocks[cipherBlocks.length-1], BLOCKSIZE -1 -paddingLength);
     }
 
     private static int sendRequest(String query) {
@@ -57,12 +62,12 @@ public class PaddingOracle {
 
     private static byte[][] splitArray(byte[] data) {
         final int length = data.length;
-        final byte[][] dest = new byte[(length + 16 - 1) / 16][];
+        final byte[][] dest = new byte[(length + BLOCKSIZE - 1) / BLOCKSIZE][];
         int destIndex = 0;
         int stopIndex = 0;
 
-        for (int startIndex = 0; startIndex + 16 <= length; startIndex += 16) {
-            stopIndex += 16;
+        for (int startIndex = 0; startIndex + BLOCKSIZE <= length; startIndex += BLOCKSIZE) {
+            stopIndex += BLOCKSIZE;
             dest[destIndex++] = Arrays.copyOfRange(data, startIndex, stopIndex);
         }
 
@@ -104,5 +109,44 @@ public class PaddingOracle {
             e.printStackTrace();
             return "";
         }
+    }
+
+    private static byte[] searchR(byte[] r, byte[] y, int position){
+        byte[] rMod = r.clone();
+        for (int i = 1; i < 256; i++) {
+            byte[][] oracleRequest = {rMod, y};
+            int response = sendRequest(encode(concatArrays(oracleRequest)));
+            System.out.println(response);
+
+            if (response == 200) {
+                break;
+            } else {
+                rMod[position] = (byte) (0xff & ((int) r[position]) ^ i);
+                System.out.println(rMod[position]);
+            }
+        }
+
+        return rMod;
+    }
+
+    private static int findPaddingLength(byte[] r, byte[]y){
+        int paddingLength = 1;
+        for (int i = 0; i < 15; i++) {
+            r[i] = (byte) (0xff & ((int) r[r.length - 1] ^ 1));
+            System.out.println(Arrays.toString(r));
+
+            byte[][] oracleRequest = {r, y};
+            int response = sendRequest(encode(concatArrays(oracleRequest)));
+            System.out.println(response);
+
+            if (response == 200) {
+                r[i] = (byte) 0;
+            } else {
+                paddingLength = BLOCKSIZE - i;
+                break;
+            }
+        }
+        System.out.println(paddingLength);
+        return paddingLength;
     }
 }
